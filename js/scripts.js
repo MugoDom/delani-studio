d84e960b4e93032ed30d224da59d68236b8e2fa9
Dominic Mugo <njuemugodominic@gmail.com>
2020-10-10 13:16:19 +0300

    add jquery script to hide paragraphs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-$("#design-paragraph").hide();
+//Keep paragraph hidden
+$("#caption-1-text").hide();
+$("#caption-1-text").hide();
+$("#caption-1-text").hide();
+
+//Reveal paragaph on image click
 $(".services-design").on("click", function(){
-    $(".services-design").slideUp("slow");
-    $("#design-paragraph").show(500);
+    $(".services-design").fadeOut("slow");
+    $("#caption-1-text").show(500);
     $("#fig-design-text").animate({fontweight:"bolder", fontsize: "30px"})
+});
+
+//hide paragraph
+$("#caption-1-text", "#fig-design-text").on("click", function(){
+    $(".services-design").fadeIn();
+    $("#caption-1-text").hide(500);
+    $("#fig-design-text").animate({fontweight:"bolder", fontsize: "30px"})
+
 });
