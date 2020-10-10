c144486f1a06a2a336b00f0174743b5a729d1b94
Dominic Mugo <njuemugodominic@gmail.com>
2020-10-10 14:02:10 +0300

    add jquery script to bolden text on click

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,20 @@ $("#caption-3-text").hide();
 $(".services-design").on("click", function(){
     $(".services-design").toggle();
     $("#caption-1-text").show();
-    $("#caption-1").animate({fontweight:"bolder", fontsize: "30px"})
+    $("#caption-1").css("font-weight", "bold");
 });
 
 $(".dev-icon-design").on("click", function(){
     $(".dev-icon-design").toggle();
     $("#caption-2-text").show();
-    $("#caption-2").animate({fontweight:"bolder", fontsize: "30px"})
+    $("#caption-2").css("font-weight", "bold");
 
 });
 
 $(".products-icon-design").on("click", function(){
     $(".products-icon-design").toggle();
     $("#caption-3-text").show();
-    $("#caption-3").animate({fontweight:"bolder", fontsize: "30px"})
+    $("#caption-3").css("font-weight", "bold");
 
 });
 
@@ -31,20 +31,20 @@ $(".products-icon-design").on("click", function(){
 $("#caption-1-text, #caption-1").on("click", function(){
     $(".services-design").fadeIn();
     $("#caption-1-text").hide(500);
-    $("#caption-1").animate({fontweight:"normal", fontsize: "20px"})
+    $("#caption-1").css("font-weight", "normal");
     
 });
 
 $("#caption-2-text, #caption-2").on("click", function(){
     $(".dev-icon-design").fadeIn();
     $("#caption-2-text").hide(500);
-    $("#caption-2").animate({fontweight:"normal", fontsize: "20px"})
+    $("#caption-2").css("font-weight", "normal");
     
 });
 
 $("#caption-3-text, #caption-3").on("click", function(){
     $(".products-icon-design").fadeIn();
     $("#caption-3-text").hide(500);
-    $("#caption-3").animate({fontweight:"normal", fontsize: "20px"})
+    $("#caption-3").css("font-weight", "normal");
     
 });
