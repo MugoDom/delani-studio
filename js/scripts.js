abd7685b3e7553524137a7c987cc26c7bcb0bbd0
Dominic Mugo <njuemugodominic@gmail.com>
2020-10-10 13:49:24 +0300

    add jquery script to animate paragraph and icon appearance

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,50 @@
 //Keep paragraph hidden
 $("#caption-1-text").hide();
-$("#caption-1-text").hide();
-$("#caption-1-text").hide();
+$("#caption-2-text").hide();
+$("#caption-3-text").hide();
 
 //Reveal paragaph on image click
 $(".services-design").on("click", function(){
-    $(".services-design").fadeOut("slow");
-    $("#caption-1-text").show(500);
-    $("#fig-design-text").animate({fontweight:"bolder", fontsize: "30px"})
+    $(".services-design").toggle();
+    $("#caption-1-text").show();
+    $("#caption-1").animate({fontweight:"bolder", fontsize: "30px"})
 });
 
+$(".dev-icon-design").on("click", function(){
+    $(".dev-icon-design").toggle();
+    $("#caption-2-text").show();
+    $("#caption-2").animate({fontweight:"bolder", fontsize: "30px"})
+
+});
+
+$(".products-icon-design").on("click", function(){
+    $(".products-icon-design").toggle();
+    $("#caption-3-text").show();
+    $("#caption-3").animate({fontweight:"bolder", fontsize: "30px"})
+
+});
+
+
+
+
 //hide paragraph
-$("#caption-1-text", "#fig-design-text").on("click", function(){
+$("#caption-1-text, #caption-1").on("click", function(){
     $(".services-design").fadeIn();
     $("#caption-1-text").hide(500);
-    $("#fig-design-text").animate({fontweight:"bolder", fontsize: "30px"})
+    $("#caption-1").animate({fontweight:"normal", fontsize: "20px"})
+    
+});
 
+$("#caption-2-text, #caption-2").on("click", function(){
+    $(".dev-icon-design").fadeIn();
+    $("#caption-2-text").hide(500);
+    $("#caption-2").animate({fontweight:"normal", fontsize: "20px"})
+    
+});
+
+$("#caption-3-text, #caption-3").on("click", function(){
+    $(".products-icon-design").fadeIn();
+    $("#caption-3-text").hide(500);
+    $("#caption-3").animate({fontweight:"normal", fontsize: "20px"})
+    
 });
